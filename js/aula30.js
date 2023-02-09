fc307bf9ce56182fa8b869dfb41fcad32f8f0bd7
tatiuska <tfweska@gmail.com>
2023-02-09 16:16:25 -0300

    Mensagens Flutuantes

---
+++ js/aula30.js
@@ -0,0 +1,26 @@
+//script para inicializar o elemento Toast
+const toastElList = document.querySelectorAll(".toast");
+const toastList = [...toastElList].map((toastEl) => {
+    const toast = new bootstrap.Toast(toastEl, {});
+    toast.show();
+});
+
+//função para criar novos toasts a partir do botão "Criar Toast"
+const btnToast = document.getElementById("btnToast");
+btnToast.addEventListener("click", () => {
+    const toast = document.getElementById("toast");
+    const container = document.getElementById("toastContainer");
+    const novoToast = toast.cloneNode(true);
+    novoToast.lastElementChild.innerHTML = "Mensagem em " + Date();
+    container.appendChild(novoToast);
+    const bsToast = new bootstrap.Toast(novoToast, {});
+    bsToast.show();
+});
+
+//cria tooltips a partir do elemento data-bs-toggle="tooltip"
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+
+//cria popovers a partir do elemento data-bs-toggle="popover"
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
